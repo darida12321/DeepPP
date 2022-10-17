8aac748b708afe97bb8c0099512b054382fd28b5
darida12321 <dani.mihalik@gmail.com>
2022-10-17 17:50:56 +0100

    refactor: move activation functions to their own file

---
+++ include/activation_function.h
@@ -0,0 +1,12 @@
+#pragma once
+#include <cmath>
+
+inline double sigmoid(double x) { return 1 / (1 + std::exp(-x)); }
+inline double sigmoid_derivative(double x) { return sigmoid(x) * (1 - sigmoid(x)); }
+
+inline double relu(double x) { return fmax(x, 0); }
+inline double relu_derivative(double x) {
+  if (x <= 0)
+    return 0;
+  return 1;
+}
