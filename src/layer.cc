0be79cd046ab5fd876dbead39c515c4a5e2d434c
Wojtek <ws420@ic.ac.uk>
2022-10-17 14:42:28 +0100

    [ws] fix: reformatted layer.h and .cc files to avoid redefining the class

--- src/layer.cc
+++ src/layer.cc
@@ -1,27 +1,20 @@
+#include "layer.h"
 #include "../Eigen/Dense"
 
 using Eigen::MatrixXd;
 using Eigen::VectorXd;
 
-class Layer {
-public:
-  // constructor
-  Layer(MatrixXd m, VectorXd b, std::function<double(double)> act_func)
-      : weights_(m), bias_(b), act_func_(act_func) {}
+// constructor
+Layer::Layer(MatrixXd m, VectorXd b, std::function<double(double)> act_func)
+    : weights_(m), bias_(b), act_func_(act_func) {}
 
-  VectorXd forwardProp(VectorXd in) {
-    VectorXd newV = weights_ * in + bias_;
-    newV = newV.unaryExpr(act_func_);
-    return newV;
-  }
+VectorXd Layer::forwardProp(VectorXd in) {
+  VectorXd newV = weights_ * in + bias_;
+  newV = newV.unaryExpr(act_func_);
+  return newV;
+}
 
-  VectorXd backProp(VectorXd err) {
-    VectorXd error = err;
-    return error;
-  }
-
-private:
-  MatrixXd weights_;
-  VectorXd bias_;
-  std::function<double(double)> act_func_; // activator function
-};
+VectorXd Layer::backProp(VectorXd err) {
+  VectorXd error = err;
+  return error;
+}
--- src/layer.h
+++ src/layer.h
@@ -1,11 +1,18 @@
 #pragma once
 #include "../Eigen/Dense"
 
+using Eigen::MatrixXd;
+using Eigen::VectorXd;
+
 class Layer {
 
+public:
   Layer(MatrixXd m, VectorXd b, std::function<double(double)> act_func);
-
   VectorXd forwardProp(VectorXd in);
-
   VectorXd backProp(VectorXd err);
+
+private:
+  MatrixXd weights_;
+  VectorXd bias_;
+  std::function<double(double)> act_func_; // activator function
 };
