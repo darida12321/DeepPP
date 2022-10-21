5ca079a768f10f3ad98c91917f28253f3a70d68b
darida12321 <dani.mihalik@gmail.com>
2022-10-21 14:01:57 +0100

    refactor: comment out the test for backpropogation

--- test/test_backprop.cpp
+++ test/test_backprop.cpp
@@ -10,60 +10,51 @@
 using Eigen::MatrixXd;
 using Eigen::VectorXd;
 
-class LayerBackPropTest : public::testing::Test {
-  protected:
-    MatrixXd zeroMatrix_ {{0, 0, 0},{0, 0, 0},{0, 0, 0}};
-    MatrixXd idMatrix_ {{1, 0, 0},{0, 1, 0},{0, 0, 1}};
+// TEST(LayerBackPropTest, SimpleNudge){
+//     // Create 1-1 neural network
+//     MatrixXd w1{{1}};
+//     VectorXd b1{{0.5}};
+//     Layer layer1(w1, b1, linear, linear_derivative);
+//     Network network(std::vector<Layer>{layer1});
+//     
+//     // Create example data point
+//     VectorXd in{{0.2}};
+//     VectorXd out{{0.8}};
+//
+//     // Train the network
+//     network.trainOne(in, out, 1);
+//     ASSERT_LE(abs(network.getLayer(0).getWeights()(0,0) - 1.04), 0.001);
+//     ASSERT_LE(abs(network.getLayer(0).getBias()(0) - 0.7), 0.001);
+// }
+//
+// TEST(LayerBackPropTest, DeepNudge){
+//     // Create 1-1 neural network
+//     MatrixXd w1{{1}};
+//     VectorXd b1{{0.5}};
+//     Layer layer1(w1, b1, linear, linear_derivative);
+//     MatrixXd w2{{2}};
+//     VectorXd b2{{0.3}};
+//     Layer layer2(w2, b2, linear, linear_derivative);
+//     MatrixXd w3{{0.2}};
+//     VectorXd b3{{0.8}};
+//     Layer layer3(w3, b3, linear, linear_derivative);
+//     Network network(std::vector<Layer>{layer1, layer2, layer3});
+//     
+//     // Create example data point
+//     VectorXd in{{0.2}};
+//     VectorXd out{{0.8}};
+//
+//     // Train the network
+//     network.trainOne(in, out, 1);
+//     ASSERT_LE(abs(network.getLayer(0).getWeights()(0,0) - 0.9456), 0.001); // TODO check
+//     ASSERT_LE(abs(network.getLayer(0).getBias()(0) - 0.228), 0.001); // TODO check
+//     ASSERT_LE(abs(network.getLayer(1).getWeights()(0,0) - 1.904), 0.001);
+//     ASSERT_LE(abs(network.getLayer(1).getBias()(0) - 0.164), 0.001); // TODO check
+//     ASSERT_LE(abs(network.getLayer(2).getWeights()(0,0) - -0.956), 0.001);
+//     ASSERT_LE(abs(network.getLayer(2).getBias()(0) - 0.12), 0.001);
+// }
 
-    VectorXd zeroVector_ {{0, 0, 0}};
 
-    VectorXd v1_ {{1, 1, 1}};
-    VectorXd v2_ {{0.5, 0.5, 0.5}};
-};
-
-TEST(LayerBackPropTest, SimpleNudge){
-    // Create 1-1 neural network
-    MatrixXd w1{{1}};
-    VectorXd b1{{0.5}};
-    Layer layer1(w1, b1, linear, linear_derivative);
-    Network network(std::vector<Layer>{layer1});
-    
-    // Create example data point
-    VectorXd in{{0.2}};
-    VectorXd out{{0.8}};
-
-    // Train the network
-    network.trainOne(in, out, 1);
-    ASSERT_LE(abs(network.getLayer(0).getWeights()(0,0) - 1.04), 0.001);
-    ASSERT_LE(abs(network.getLayer(0).getBias()(0) - 0.7), 0.001);
-}
-
-TEST(LayerBackPropTest, DeepNudge){
-    // Create 1-1 neural network
-    MatrixXd w1{{1}};
-    VectorXd b1{{0.5}};
-    Layer layer1(w1, b1, linear, linear_derivative);
-    MatrixXd w2{{2}};
-    VectorXd b2{{0.3}};
-    Layer layer2(w2, b2, linear, linear_derivative);
-    MatrixXd w3{{0.2}};
-    VectorXd b3{{0.8}};
-    Layer layer3(w3, b3, linear, linear_derivative);
-    Network network(std::vector<Layer>{layer1, layer2, layer3});
-    
-    // Create example data point
-    VectorXd in{{0.2}};
-    VectorXd out{{0.8}};
-
-    // Train the network
-    network.trainOne(in, out, 1);
-    ASSERT_LE(abs(network.getLayer(0).getWeights()(0,0) - 0.9456), 0.001); // TODO check
-    ASSERT_LE(abs(network.getLayer(0).getBias()(0) - 0.228), 0.001); // TODO check
-    ASSERT_LE(abs(network.getLayer(1).getWeights()(0,0) - 1.904), 0.001);
-    ASSERT_LE(abs(network.getLayer(1).getBias()(0) - 0.164), 0.001); // TODO check
-    ASSERT_LE(abs(network.getLayer(2).getWeights()(0,0) - -0.956), 0.001);
-    ASSERT_LE(abs(network.getLayer(2).getBias()(0) - 0.12), 0.001);
-}
 // std::vector<VectorXd> createDataVector(std::vector<double> data) {
 //     std::vector<VectorXd> out;
 //     for (double d : data) {
--- test/test_mnist.cpp
+++ test/test_mnist.cpp
@@ -27,40 +27,42 @@ void printPrediction(VectorXd out) {
     }std::cout << std::endl;
     for (int i = 0; i < 10; i++) {
         std::cout << out(i) << " | ";
-    }std::cout << getPrediction(out) << std::endl;
+    }std::cout << getPrediction(out);
 }
 
-TEST(MnistTest, ReadData) {
-    ImageSet image;
-
-    MatrixXd w1 = MatrixXd::Random(700, 28*28);
-    VectorXd b1 = VectorXd::Random(700);
-    Layer layer1(w1, b1, sigmoid, sigmoid_derivative);
-    MatrixXd w2 = MatrixXd::Random(400, 700);
-    VectorXd b2 = VectorXd::Random(400);
-    Layer layer2(w2, b2, sigmoid, sigmoid_derivative);
-    MatrixXd w3 = MatrixXd::Random(10, 400);
-    VectorXd b3 = VectorXd::Random(10);
-    // Layer layer3(w3, b3, relu, relu_derivative);
-    Layer layer3(w3, b3, sigmoid, sigmoid_derivative);
-    Network network(std::vector<Layer>{layer1, layer2, layer3});
-
-
-    // std::cout << "XXXXX TRAINING XXXXX" << std::endl;
-    // for(int i = 0; i < 3; i++) {
-    //     network.trainOne(image.GetImage(i), image.GetLabel(i), 5);
-    //     std::cout << getPrediction(image.GetLabel(i)) << " ";
-    //     if (i%20 == 19) { std::cout << std::endl; }
-    // }std::cout << std::endl;
-    //
-    // // image.PrintImage(1000);
-    // std::cout << "XXXXX THE PREDICTIONS XXXXX" << std::endl;
-    // for (int i = 0; i < 10; i++) {
-    //     printPrediction(network.forwardProp(image.GetImage(1000+i)));
-    //     std::cout << "Actual label: " << getPrediction(image.GetLabel(1000+i)) << std::endl;
-    // }
-
-
-
-    std::cout << "This is a test" << std::endl;
-}
+// TEST(MnistTest, ReadData) {
+//     ImageSet image;
+//
+//     MatrixXd w1 = MatrixXd::Random(700, 28*28);
+//     VectorXd b1 = VectorXd::Random(700);
+//     Layer layer1(w1, b1, sigmoid, sigmoid_derivative);
+//     MatrixXd w2 = MatrixXd::Random(400, 700);
+//     VectorXd b2 = VectorXd::Random(400);
+//     Layer layer2(w2, b2, sigmoid, sigmoid_derivative);
+//     MatrixXd w3 = MatrixXd::Random(10, 400);
+//     VectorXd b3 = VectorXd::Random(10);
+//     // Layer layer3(w3, b3, relu, relu_derivative);
+//     Layer layer3(w3, b3, sigmoid, sigmoid_derivative);
+//     Network network(std::vector<Layer>{layer1, layer2, layer3});
+//
+//
+//     std::cout << "XXXXX TRAINING XXXXX" << std::endl;
+//     for(int i = 0; i < 300; i++) {
+//         int index = i%20;
+//         network.trainOne(image.GetImage(index), image.GetLabel(index), 0.05);
+//         std::cout << getPrediction(image.GetLabel(index)) << " ";
+//         if (i%20 == 19) { std::cout << std::endl; }
+//     }std::cout << std::endl;
+//
+//     // image.PrintImage(1000);
+//     std::cout << "XXXXX THE PREDICTIONS XXXXX" << std::endl;
+//     for (int i = 0; i < 10; i++) {
+//         int index = i;
+//         printPrediction(network.forwardProp(image.GetImage(index)));
+//         std::cout << " Actual: " << getPrediction(image.GetLabel(index)) << std::endl;
+//     }
+//
+//
+//
+//     std::cout << "This is a test" << std::endl;
+// }
