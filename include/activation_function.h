bd4b907d810e3f8671c0ded811450f8155ec167c
darida12321 <dani.mihalik@gmail.com>
2022-10-23 11:09:26 +0100

    feat: implement softmax activation function

--- include/activation_function.h
+++ include/activation_function.h
@@ -1,15 +1,52 @@
 #pragma once
 #include <cmath>
+#include <Eigen/Dense>
 
-inline double sigmoid(double x) { return 1 / (1 + std::exp(-x)); }
-inline double sigmoid_derivative(double x) { return sigmoid(x) * (1 - sigmoid(x)); }
+using Eigen::MatrixXd;
+using Eigen::VectorXd;
 
-inline double relu(double x) { return fmax(x, 0); }
-inline double relu_derivative(double x) {
-  if (x <= 0)
-    return 0;
-  return 1;
+inline VectorXd sigmoid(VectorXd x) {
+  return x.unaryExpr([](double x){ return 1/(1+std::exp(-x)); });
+}
+inline VectorXd sigmoid_derivative(VectorXd x) {
+  return sigmoid(x).cwiseProduct(sigmoid(x).unaryExpr([](double x){ return 1-x; }));
 }
 
-inline double linear(double x) { return x; }
-inline double linear_derivative(double x) { return 1; }
+inline VectorXd softmax(VectorXd x) {
+  double max = x.maxCoeff();
+  VectorXd shiftx = x.array() - max;
+  VectorXd exps = shiftx.unaryExpr([](double x){ return std::exp(x); });
+  return exps.array() / exps.sum();
+}
+inline VectorXd softmax_derivative(VectorXd x) {
+
+}
+
+inline VectorXd relu(VectorXd x) {
+  return x.unaryExpr([](double x){ return fmax(x, 0); });
+}
+inline VectorXd relu_derivative(VectorXd x) {
+  return x.unaryExpr([](double x){
+    if (x <= 0)
+      return 0.0;
+    return 1.0;
+  });
+}
+
+inline VectorXd linear(VectorXd x) { return x; }
+inline VectorXd linear_derivative(VectorXd x) {
+  return VectorXd::Ones(x.rows(), x.cols());
+}
+
+// inline double sigmoid(double x) { return 1 / (1 + std::exp(-x)); }
+// inline double sigmoid_derivative(double x) { return sigmoid(x) * (1 - sigmoid(x)); }
+//
+// inline double relu(double x) { return fmax(x, 0); }
+// inline double relu_derivative(double x) {
+//   if (x <= 0)
+//     return 0;
+//   return 1;
+// }
+//
+// inline double linear(double x) { return x; }
+// inline double linear_derivative(double x) { return 1; }
