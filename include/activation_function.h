aef809ad031463dbc159907f335b45ec4e662070
Wojtek <ws420@ic.ac.uk>
2022-10-29 22:51:10 +0100

    removed commented code, added readme to the doxyfile

--- include/activation_function.h
+++ include/activation_function.h
@@ -99,17 +99,3 @@ inline VectorXd linear(VectorXd x) { return x; }
 inline VectorXd linear_derivative(VectorXd x) {
   return VectorXd::Ones(x.rows(), x.cols());
 }
-
-// inline double sigmoid(double x) { return 1 / (1 + std::exp(-x)); }
-// inline double sigmoid_derivative(double x) { return sigmoid(x) * (1 -
-// sigmoid(x)); }
-//
-// inline double relu(double x) { return fmax(x, 0); }
-// inline double relu_derivative(double x) {
-//   if (x <= 0)
-//     return 0;
-//   return 1;
-// }
-//
-// inline double linear(double x) { return x; }
-// inline double linear_derivative(double x) { return 1; }
