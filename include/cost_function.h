8cc632cc9147f7b05c9c3bcf0cc1e203c2983bb5
Wojtek <ws420@ic.ac.uk>
2022-10-31 11:32:48 +0000

    [ws] fix: fixed mistake in mean_sqr_error and benchmarks

--- include/cost_function.h
+++ include/cost_function.h
@@ -9,7 +9,7 @@ using Eigen::VectorXd;
 
 inline double mean_sqr_error(VectorXd out, VectorXd exp_out) {
   auto errors = (out - exp_out).array();
-  return (errors * errors).sum() / out.rows();
+  return (errors * errors).sum();
 }
 
 inline VectorXd mean_sqr_error_der(VectorXd out, VectorXd exp_out) {
