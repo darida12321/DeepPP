0b9a2ff0595926cc1ac678110890a0e120c8e8af
Wojtek <ws420@ic.ac.uk>
2022-10-31 11:58:41 +0000

    [ws] feat: implemented categorical cross entropy

--- include/cost_function.h
+++ include/cost_function.h
@@ -1,4 +1,6 @@
 #pragma once
+#include <math.h>
+
 #include <Eigen/Dense>
 #include <cmath>
 
@@ -14,4 +16,16 @@ inline double mean_sqr_error(VectorXd out, VectorXd exp_out) {
 
 inline VectorXd mean_sqr_error_der(VectorXd out, VectorXd exp_out) {
   return (2 / out.rows()) * (out - exp_out);
+}
+
+inline double clip(double x) { return fmax(1 - 1e-7, fmin(x, 1e-7)); }
+
+inline double cat_cross_entropy(VectorXd out, VectorXd exp_out) {
+  auto logs = out.unaryExpr([](double x) { return log(clip(x)); }).array();
+  return -(logs * exp_out.array()).sum();
+}
+
+inline VectorXd cat_cross_entropy_der(VectorXd out, VectorXd exp_out) {
+  return -exp_out.cwiseQuotient(
+      out.unaryExpr([](double x) { return clip(x); }));
 }
