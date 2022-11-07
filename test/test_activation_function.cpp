b6d280c882d8bed1fab51933e10d5c613f6629f6
N00bAdrian <N00bAdrian@users.noreply.github.com>
2022-11-07 19:07:43 +0000

    Reformat code by workflow

--- test/test_activation_function.cpp
+++ test/test_activation_function.cpp
@@ -1,52 +1,51 @@
+#include <activation_function.h>
+#include <gtest/gtest.h>
 #include <templates/activation_function.h>
 #include <templates/linalg.h>
-#include <activation_function.h>
-
-#include <gtest/gtest.h>
 
 using Eigen::MatrixXd;
 using Eigen::VectorXd;
 
 TEST(ActivationFunction, Sigmoid) {
-	for (int i = 0; i < 1000; i++){
-		VectorXd v = VectorXd::Random(10);
-		Vectord<10> v_t(v);
-		Template::Sigmoid<10> sigmoid_t;
+  for (int i = 0; i < 1000; i++) {
+    VectorXd v = VectorXd::Random(10);
+    Vectord<10> v_t(v);
+    Template::Sigmoid<10> sigmoid_t;
 
-		EXPECT_TRUE(sigmoid.function(v).isApprox(sigmoid_t.function(v_t)));
-		EXPECT_TRUE(sigmoid.derivative(v).isApprox(sigmoid_t.derivative(v_t)));
-	}
+    EXPECT_TRUE(sigmoid.function(v).isApprox(sigmoid_t.function(v_t)));
+    EXPECT_TRUE(sigmoid.derivative(v).isApprox(sigmoid_t.derivative(v_t)));
+  }
 }
 
 TEST(ActivationFunction, Softmax) {
-	for(int i = 0; i < 1000; i++){
-		VectorXd v = VectorXd::Random(10);
-		Vectord<10> v_t(v);
-		Template::Softmax<10> softmax_t;
+  for (int i = 0; i < 1000; i++) {
+    VectorXd v = VectorXd::Random(10);
+    Vectord<10> v_t(v);
+    Template::Softmax<10> softmax_t;
 
-		EXPECT_TRUE(softmax.function(v).isApprox(softmax_t.function(v_t)));
-		EXPECT_TRUE(softmax.derivative(v).isApprox(softmax_t.derivative(v_t)));
-	}
+    EXPECT_TRUE(softmax.function(v).isApprox(softmax_t.function(v_t)));
+    EXPECT_TRUE(softmax.derivative(v).isApprox(softmax_t.derivative(v_t)));
+  }
 }
 
 TEST(ActivationFunction, Relu) {
-	for (int i = 0; i < 1000; i++){
-		VectorXd v = VectorXd::Random(10);
-		Vectord<10> v_t(v);
-		Template::Relu<10> relu_t;
+  for (int i = 0; i < 1000; i++) {
+    VectorXd v = VectorXd::Random(10);
+    Vectord<10> v_t(v);
+    Template::Relu<10> relu_t;
 
-		EXPECT_TRUE(relu.function(v).isApprox(relu_t.function(v_t)));
-		EXPECT_TRUE(relu.derivative(v).isApprox(relu_t.derivative(v_t)));
-	}
+    EXPECT_TRUE(relu.function(v).isApprox(relu_t.function(v_t)));
+    EXPECT_TRUE(relu.derivative(v).isApprox(relu_t.derivative(v_t)));
+  }
 }
 
 TEST(ActivationFunction, Linear) {
-	for (int i = 0; i < 1000; i++){
-		VectorXd v = VectorXd::Random(10);
-		Vectord<10> v_t(v);
-		Template::Linear<10> linear_t;
+  for (int i = 0; i < 1000; i++) {
+    VectorXd v = VectorXd::Random(10);
+    Vectord<10> v_t(v);
+    Template::Linear<10> linear_t;
 
-		EXPECT_TRUE(linear.function(v).isApprox(linear_t.function(v_t)));
-		EXPECT_TRUE(linear.derivative(v).isApprox(linear_t.derivative(v_t)));
-	}
+    EXPECT_TRUE(linear.function(v).isApprox(linear_t.function(v_t)));
+    EXPECT_TRUE(linear.derivative(v).isApprox(linear_t.derivative(v_t)));
+  }
 }
