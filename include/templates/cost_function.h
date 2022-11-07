5dbccf12b2bdfd60f177c6f341b2ef6302f5400e
Wojtek <ws420@ic.ac.uk>
2022-11-07 22:44:27 +0000

    [ws] feat: created a templatised version of cost_function.h

---
+++ include/templates/cost_function.h
@@ -0,0 +1,44 @@
+#pragma once
+#include <math.h>
+#include <templates/linalg.h>
+
+#include <Eigen/Dense>
+#include <cmath>
+
+using Eigen::MatrixXd;
+using Eigen::VectorXd;
+
+namespace Template {
+template <int size>
+class CostFunction {
+ public:
+  virtual inline double function(Vectord<size> out, Vectord<size> exp_out) = 0;
+  virtual inline Vectord<size> derivative(Vectord<size> out,
+                                          Vectord<size> exp_out) = 0;
+};
+
+template <int size>
+class MeanSquareError : public CostFunction<size> {
+  inline double function(Vectord<size> out, Vectord<size> exp_out) {
+    auto errors = (out - exp_out).array();
+    return (errors * errors).sum() / out.rows();
+  }
+  inline VectorXd derivative(Vectord<size> out, Vectord<size> exp_out) {
+    return (2.0 / out.rows()) * (out - exp_out);
+  }
+};
+
+inline double clip(double x) { return fmin(1 - 1e-7, fmax(x, 1e-7)); }
+
+template <int size>
+class CategoricalCrossEntropy : public CostFunction<size> {
+  inline double function(Vectord<size> out, Vectord<size> exp_out) {
+    auto logs = out.unaryExpr([](double x) { return log(clip(x)); }).array();
+    return -(logs * exp_out.array()).sum();
+  }
+  inline VectorXd derivative(Vectord<size> out, Vectord<size> exp_out) {
+    return -exp_out.cwiseQuotient(
+        out.unaryExpr([](double x) { return clip(x); }));
+  }
+};
+}  // namespace Template
