dc999ba5652cf6ff36773f9bc3a6b258bfb3ee1b
Wojtek <ws420@ic.ac.uk>
2022-11-08 10:19:17 +0000

    [ws] doc: documented cost_function.h

--- include/templates/cost_function.h
+++ include/templates/cost_function.h
@@ -6,38 +6,76 @@
 #include <cmath>
 
 namespace Template {
-  template <int size>
-  class CostFunction {
-  public:
-    virtual inline double function(Vectord<size> out, Vectord<size> exp_out) = 0;
-    virtual inline Vectord<size> derivative(Vectord<size> out,
-                                            Vectord<size> exp_out) = 0;
-  };
+template <int size>
+class CostFunction {
+ public:
+  virtual inline double function(Vectord<size> out, Vectord<size> exp_out) = 0;
+  virtual inline Vectord<size> derivative(Vectord<size> out,
+                                          Vectord<size> exp_out) = 0;
+};
 
-  template <int size>
-  class MeanSquareError : public CostFunction<size> {
-    public:
-      inline double function(Vectord<size> out, Vectord<size> exp_out) {
-        auto errors = (out - exp_out).array();
-        return (errors * errors).sum() / out.rows();
-      }
-      inline Vectord<size> derivative(Vectord<size> out, Vectord<size> exp_out) {
-        return (2.0 / out.rows()) * (out - exp_out);
-      }
-  };
+template <int size>
+class MeanSquareError : public CostFunction<size> {
+ public:
+  /**
+   * @brief Calculate the mean square error
+   *
+   * @param out The actual output of the neural network
+   * @param exp_out The expected output
+   * @return double The mean square error
+   */
+  inline double function(Vectord<size> out, Vectord<size> exp_out) {
+    auto errors = (out - exp_out).array();
+    return (errors * errors).sum() / size;
+  }
+  /**
+   * @brief Calculate the gradient of the mean square error
+   *
+   * @param out The actual output of the neural network
+   * @param exp_out The expected output
+   * @return Vectord<size> Vector containing the partial derrivatives of the
+   * mean square error
+   */
+  inline Vectord<size> derivative(Vectord<size> out, Vectord<size> exp_out) {
+    return (2.0 / size) * (out - exp_out);
+  }
+};
 
-  inline double clip(double x) { return fmin(1 - 1e-7, fmax(x, 1e-7)); }
+/**
+ * @brief restricts values from the closed interval [0,1] to the open interval
+ * (0,1). Used in categorical cross entropy to prevent division by 0.
+ *
+ * @param x
+ * @return double
+ * @pre 0 <= x <= 1
+ */
+inline double clip(double x) { return fmin(1 - 1e-7, fmax(x, 1e-7)); }
 
-  template <int size>
-  class CategoricalCrossEntropy : public CostFunction<size> {
-    public:
-      inline double function(Vectord<size> out, Vectord<size> exp_out) {
-        auto logs = out.unaryExpr([](double x) { return log(clip(x)); }).array();
-        return -(logs * exp_out.array()).sum();
-      }
-      inline Vectord<size> derivative(Vectord<size> out, Vectord<size> exp_out) {
-        return -exp_out.cwiseQuotient(
-            out.unaryExpr([](double x) { return clip(x); }));
-      }
-  };
+template <int size>
+class CategoricalCrossEntropy : public CostFunction<size> {
+ public:
+  /**
+   * @brief Calculate the categorical cross entropy
+   *
+   * @param out The actual output of the neural network
+   * @param exp_out The expected output
+   * @return double The categorical cross entropy
+   */
+  inline double function(Vectord<size> out, Vectord<size> exp_out) {
+    auto logs = out.unaryExpr([](double x) { return log(clip(x)); }).array();
+    return -(logs * exp_out.array()).sum();
+  }
+  /**
+   * @brief Calculate the gradient of the categorical cross entropy
+   *
+   * @param out The actual output of the neural network
+   * @param exp_out The expected output
+   * @return Vectord<size> Vector containing the partial derivatives of the
+   * categorical cross entropy
+   */
+  inline Vectord<size> derivative(Vectord<size> out, Vectord<size> exp_out) {
+    return -exp_out.cwiseQuotient(
+        out.unaryExpr([](double x) { return clip(x); }));
+  }
+};
 }  // namespace Template
