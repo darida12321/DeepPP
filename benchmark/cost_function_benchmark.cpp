b7a23f8358452f0147fefbc2beb5216b82966c58
N00bAdrian <adrianwong227@gmail.com>
2022-11-08 02:16:12 +0000

    [aw] test: add benchmarks for cost functions

---
+++ benchmark/cost_function_benchmark.cpp
@@ -0,0 +1,49 @@
+#include <cost_function.h>
+#include <templates/linalg.h>
+#include <templates/cost_function.h>
+
+#include <benchmark/benchmark.h>
+
+static void BM_MeanSquareError(benchmark::State& state) {
+	VectorXd v = VectorXd::Random(10);
+	MeanSquareError mse;
+	for (auto _ : state) {
+		mse.function(v, v);
+		mse.derivative(v, v);
+	}
+}
+
+BENCHMARK(BM_MeanSquareError);
+
+static void BM_Template_MeanSquareError(benchmark::State& state) {
+	Vectord<10> v = Vectord<10>::Random();
+	Template::MeanSquareError<10> mse;
+	for (auto _ : state) {
+		mse.function(v, v);
+		mse.derivative(v, v);
+	}
+}
+
+BENCHMARK(BM_Template_MeanSquareError);
+
+static void BM_CategoricalCrossEntropy(benchmark::State& state) {
+	VectorXd v = VectorXd::Random(10);
+	CategoricalCrossEntropy cce;
+	for (auto _ : state) {
+		cce.function(v, v);
+		cce.derivative(v, v);
+	}
+}
+
+BENCHMARK(BM_CategoricalCrossEntropy);
+
+static void BM_Template_CategoricalCrossEntropy(benchmark::State& state) {
+	Vectord<10> v = Vectord<10>::Random();
+	Template::CategoricalCrossEntropy<10> cce;
+	for (auto _ : state) {
+		cce.function(v, v);
+		cce.derivative(v, v);
+	}
+}
+
+BENCHMARK(BM_Template_CategoricalCrossEntropy);
