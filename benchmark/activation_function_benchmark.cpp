26463129e0bc01ac47041c2b7e398d7c748ed421
N00bAdrian <adrianwong227@gmail.com>
2022-11-08 02:20:08 +0000

    [aw] refactor: combine benchmarks for runtime and template activation functions

--- benchmark/activation_function_benchmark.cpp
+++ benchmark/activation_function_benchmark.cpp
@@ -1,5 +1,6 @@
 
 #include <activation_function.h>
+#include <templates/activation_function.h>
 #include <benchmark/benchmark.h>
 #include <templates/linalg.h>
 
@@ -15,6 +16,18 @@ static void BM_Sigmoid(benchmark::State& state) {
 
 BENCHMARK(BM_Sigmoid);
 
+static void BM_Template_Sigmoid(benchmark::State& state) {
+  Vectord<1> v;
+  v << 1;
+  Template::Sigmoid<1> s;
+  for (auto _ : state) {
+    s.function(v);
+    s.derivative(v);
+  }
+}
+
+BENCHMARK(BM_Template_Sigmoid);
+
 static void BM_Softmax(benchmark::State& state) {
   VectorXd v(1);
   v << 1;
@@ -26,6 +39,18 @@ static void BM_Softmax(benchmark::State& state) {
 }
 
 BENCHMARK(BM_Softmax);
+
+static void BM_Template_Softmax(benchmark::State& state) {
+  Vectord<1> v;
+  v << 1;
+  Template::Softmax<1> s;
+  for (auto _ : state) {
+    s.function(v);
+    s.derivative(v);
+  }
+}
+
+BENCHMARK(BM_Template_Softmax);
 
 static void BM_Relu(benchmark::State& state) {
   VectorXd v(1);
@@ -39,6 +64,18 @@ static void BM_Relu(benchmark::State& state) {
 
 BENCHMARK(BM_Relu);
 
+static void BM_Template_Relu(benchmark::State& state) {
+  Vectord<1> v;
+  v << 1;
+  Template::Relu<1> r;
+  for (auto _ : state) {
+    r.function(v);
+    r.derivative(v);
+  }
+}
+
+BENCHMARK(BM_Template_Relu);
+
 static void BM_Linear(benchmark::State& state) {
   VectorXd v(1);
   v << 1;
@@ -50,3 +87,15 @@ static void BM_Linear(benchmark::State& state) {
 }
 
 BENCHMARK(BM_Linear);
+
+static void BM_Template_Linear(benchmark::State& state) {
+  Vectord<1> v;
+  v << 1;
+  Template::Linear<1> l;
+  for (auto _ : state) {
+    l.function(v);
+    l.derivative(v);
+  }
+}
+
+BENCHMARK(BM_Template_Linear);
