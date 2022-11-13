a48e02ad2d35a0beaaa2b8f76683d4e98beaae81
N00bAdrian <adrianwong227@gmail.com>
2022-11-13 19:32:39 +0000

    [aw] test: restored tests and benchmarks for activation functions

--- benchmark/activation_function_benchmark.cpp
+++ benchmark/activation_function_benchmark.cpp
@@ -2,101 +2,96 @@
 #include <activation_function.h>
 #include <benchmark/benchmark.h>
 // TODO restore
-// #include <templates/activation_function.h>
+#include <templates/activation_function.h>
 // #include <templates/linalg.h>
 
-// static void BM_Sigmoid(benchmark::State& state) {
-//   VectorXd v(1);
-//   v << 1;
-//   Sigmoid s;
-//   for (auto _ : state) {
-//     s.function(v);
-//     s.derivative(v);
-//   }
-// }
-//
-// BENCHMARK(BM_Sigmoid);
-//
-// static void BM_Template_Sigmoid(benchmark::State& state) {
-//   Vectord<1> v;
-//   v << 1;
-//   Template::Sigmoid<1> s;
-//   for (auto _ : state) {
-//     s.function(v);
-//     s.derivative(v);
-//   }
-// }
-//
-// BENCHMARK(BM_Template_Sigmoid);
-//
-// static void BM_Softmax(benchmark::State& state) {
-//   VectorXd v(1);
-//   v << 1;
-//   Softmax s;
-//   for (auto _ : state) {
-//     s.function(v);
-//     s.derivative(v);
-//   }
-// }
-//
-// BENCHMARK(BM_Softmax);
-//
-// static void BM_Template_Softmax(benchmark::State& state) {
-//   Vectord<1> v;
-//   v << 1;
-//   Template::Softmax<1> s;
-//   for (auto _ : state) {
-//     s.function(v);
-//     s.derivative(v);
-//   }
-// }
-//
-// BENCHMARK(BM_Template_Softmax);
-//
-// static void BM_Relu(benchmark::State& state) {
-//   VectorXd v(1);
-//   v << 1;
-//   Relu r;
-//   for (auto _ : state) {
-//     r.function(v);
-//     r.derivative(v);
-//   }
-// }
-//
-// BENCHMARK(BM_Relu);
-//
-// static void BM_Template_Relu(benchmark::State& state) {
-//   Vectord<1> v;
-//   v << 1;
-//   Template::Relu<1> r;
-//   for (auto _ : state) {
-//     r.function(v);
-//     r.derivative(v);
-//   }
-// }
-//
-// BENCHMARK(BM_Template_Relu);
-//
-// static void BM_Linear(benchmark::State& state) {
-//   VectorXd v(1);
-//   v << 1;
-//   Linear l;
-//   for (auto _ : state) {
-//     l.function(v);
-//     l.derivative(v);
-//   }
-// }
-//
-// BENCHMARK(BM_Linear);
-//
-// static void BM_Template_Linear(benchmark::State& state) {
-//   Vectord<1> v;
-//   v << 1;
-//   Template::Linear<1> l;
-//   for (auto _ : state) {
-//     l.function(v);
-//     l.derivative(v);
-//   }
-// }
-//
-// BENCHMARK(BM_Template_Linear);
+using Eigen::VectorXd;
+using Eigen::Vector;
+
+static void BM_Sigmoid(benchmark::State& state) {
+  VectorXd v = VectorXd::Random(10);
+  Sigmoid s;
+  for (auto _ : state) {
+    s.function(v);
+    s.derivative(v);
+  }
+}
+
+BENCHMARK(BM_Sigmoid);
+
+static void BM_Template_Sigmoid(benchmark::State& state) {
+	Vector<double, 10> v = Vector<double, 10>::Random();
+  Template::Sigmoid<10> s;
+  for (auto _ : state) {
+    s.activation(v);
+    s.activation_der(v);
+  }
+}
+
+BENCHMARK(BM_Template_Sigmoid);
+
+static void BM_Softmax(benchmark::State& state) {
+	VectorXd v = VectorXd::Random(10);
+	Softmax s;
+	for (auto _ : state) {
+		s.function(v);
+		s.derivative(v);
+	}
+}
+
+BENCHMARK(BM_Softmax);
+
+static void BM_Template_Softmax(benchmark::State& state) {
+	Vector<double, 10> v = Vector<double, 10>::Random();
+	Template::Softmax<10> s;
+	for (auto _ : state) {
+		s.activation(v);
+		s.activation_der(v);
+	}
+}
+
+BENCHMARK(BM_Template_Softmax);
+
+static void BM_Relu(benchmark::State& state) {
+	VectorXd v = VectorXd::Random(10);
+	Relu r;
+	for (auto _ : state) {
+		r.function(v);
+		r.derivative(v);
+	}
+}
+
+BENCHMARK(BM_Relu);
+
+static void BM_Template_Relu(benchmark::State& state) {
+	Vector<double, 10> v = Vector<double, 10>::Random();
+	Template::Relu<10> r;
+	for (auto _ : state) {
+		r.activation(v);
+		r.activation_der(v);
+	}
+}
+
+BENCHMARK(BM_Template_Relu);
+
+static void BM_Linear(benchmark::State& state) {
+	VectorXd v = VectorXd::Random(10);
+	Linear l;
+	for (auto _ : state) {
+		l.function(v);
+		l.derivative(v);
+	}
+}
+
+BENCHMARK(BM_Linear);
+
+static void BM_Template_Linear(benchmark::State& state) {
+	Vector<double, 10> v = Vector<double, 10>::Random();
+	Template::Linear<10> l;
+	for (auto _ : state) {
+		l.activation(v);
+		l.activation_der(v);
+	}
+}
+
+BENCHMARK(BM_Template_Linear);
--- test/test_activation_function.cpp
+++ test/test_activation_function.cpp
@@ -1,10 +1,12 @@
 #include <activation_function.h>
+#include <templates/activation_function.h>
 #include <gtest/gtest.h>
 
 #include <Eigen/Dense>
 
 using Eigen::MatrixXd;
 using Eigen::VectorXd;
+using Eigen::Vector;
 
 TEST(ActivationFunction, Sigmoid) {
   VectorXd in(5);
@@ -40,4 +42,48 @@ TEST(ActivationFunction, Linear) {
   out << -2, -1, 0, 1, 2;
 
   EXPECT_TRUE(linear.function(in).isApprox(out, 0.001));
+}
+
+TEST(ActivationFunction, Sigmoid_Template) {
+  for (int i = 0; i < 100; i++) {
+    VectorXd in = VectorXd::Random(10);
+    Vector<double, 10> in_t(in);
+    Template::Sigmoid<10> sigmoid_t;
+
+    EXPECT_TRUE(sigmoid.function(in).isApprox(sigmoid_t.activation(in_t)));
+    EXPECT_TRUE(sigmoid.derivative(in).isApprox(sigmoid_t.activation_der(in_t)));
+  }
+}
+
+TEST(ActivationFunction, Softmax_Template) {
+  for (int i = 0; i < 100; i++) {
+    VectorXd in = VectorXd::Random(10);
+    Vector<double, 10> in_t(in);
+    Template::Softmax<10> softmax_t;
+
+    EXPECT_TRUE(softmax.function(in).isApprox(softmax_t.activation(in_t)));
+    EXPECT_TRUE(softmax.derivative(in).isApprox(softmax_t.activation_der(in_t)));
+  }
+}
+
+TEST(ActivationFunction, Relu_Template) {
+  for (int i = 0; i < 100; i++) {
+    VectorXd in = VectorXd::Random(10);
+    Vector<double, 10> in_t(in);
+    Template::Relu<10> relu_t;
+
+    EXPECT_TRUE(relu.function(in).isApprox(relu_t.activation(in_t)));
+    EXPECT_TRUE(relu.derivative(in).isApprox(relu_t.activation_der(in_t)));
+  }
+}
+
+TEST(ActivationFunction, Linear_Template) {
+  for (int i = 0; i < 100; i++) {
+    VectorXd in = VectorXd::Random(10);
+    Vector<double, 10> in_t(in);
+    Template::Linear<10> linear_t;
+
+    EXPECT_TRUE(linear.function(in).isApprox(linear_t.activation(in_t)));
+    EXPECT_TRUE(linear.derivative(in).isApprox(linear_t.activation_der(in_t)));
+  }
 }
