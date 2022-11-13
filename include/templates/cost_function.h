6f72ebded3a47b85be10a952df110d1d03e126ca
N00bAdrian <adrianwong227@gmail.com>
2022-11-13 19:13:48 +0000

    [aw] fix: added crossentropy function and restored cost function tests and benchmarks

--- include/templates/cost_function.h
+++ include/templates/cost_function.h
@@ -2,15 +2,33 @@
 
 // TODO  documentation
 //TODO add other
+namespace Template {
+  template<int N>
+  struct MeanSquareError {
+    typedef Eigen::Vector<double, N> Vec;
+    inline double cost(Vec out, Vec exp_out) {
+      auto errors = (out - exp_out).array();
+      return (errors * errors).sum() / out.rows();
+    }
+    inline Vec cost_der(Vec out, Vec exp_out) {
+      return (2.0 / out.rows()) * (out - exp_out);
+    }
+  };
 
-template<int N>
-struct MeanSquareError {
-  typedef Eigen::Vector<double, N> Vec;
-  inline double cost(Vec out, Vec exp_out) {
-    auto errors = (out - exp_out).array();
-    return (errors * errors).sum() / out.rows();
-  }
-  inline Vec cost_der(Vec out, Vec exp_out) {
-    return (2.0 / out.rows()) * (out - exp_out);
-  }
-};
+  template<int N>
+  struct CategoricalCrossEntropy {
+    typedef Eigen::Vector<double, N> Vec;
+    inline double clip(double x) { return fmin(1 - 1e-7, fmax(x, 1e-7)); }
+
+    inline double cost(Vec out, Vec exp_out) {
+      auto logs = out.unaryExpr([this](double x) { return log(clip(x)); }).array();
+      return -(logs * exp_out.array()).sum();
+    }
+
+    inline Vec cost_der(Vec out, Vec exp_out) {
+      return -exp_out.cwiseQuotient(
+          out.unaryExpr([this](double x) { return clip(x); }));
+    }
+  };
+
+}
