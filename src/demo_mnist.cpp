f2484b9cf256699e35518dce66ad6d3732def42c
darida12321 <dani.mihalik@gmail.com>
2022-11-14 15:19:27 +0000

    feat: add demo file and compilation

---
+++ src/demo_mnist.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+#include <Eigen/Dense>
+#include <templates/network.h>
+
+using Eigen::Matrix;
+using Eigen::Vector;
+using namespace Template;
+
+int main() {
+  std::cout << "Hi sussy bakas!!!" << std::endl;
+  Matrix<double, 2, 2> w1{{1, 2}, {1, 4}};
+  Vector<double, 2> b1{9, -1};
+  Matrix<double, 2, 2> w2{{1, 2}, {1, 4}};
+  Vector<double, 2> b2{9, -1};
+
+  Network<MeanSquareError, InputLayer<2>, Layer<2, Linear>, Layer<2, Softmax>>
+      network;
+  network.setWeights(w1, w2);
+  network.setBiases(b1, b2);
+
+  // Check forwardpropogation value
+  Vector<double, 2> in{2, 4};
+
+  Vector<double, 2> out = network.forwardProp(in);
+  std::cout << "Output: " << std::endl << out << std::endl;
+  return 0;
+}
