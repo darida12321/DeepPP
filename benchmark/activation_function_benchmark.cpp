0c15f147bae5c25eac4f802561d745d0fabee50e
N00bAdrian <adrianwong227@gmail.com>
2022-11-22 11:03:46 +0000

    [aw] test: force non-optimization for activation function benchmarks

--- benchmark/activation_function_benchmark.cpp
+++ benchmark/activation_function_benchmark.cpp
@@ -12,8 +12,8 @@ static void BM_Activation_Sigmoid(benchmark::State& state) {
   VectorXd v = VectorXd::Random(128);
   Sigmoid s;
   for (auto _ : state) {
-    s.function(v);
-    s.derivative(v);
+    benchmark::DoNotOptimize(s.function(v));
+    benchmark::DoNotOptimize(s.derivative(v));
   }
 }
 
@@ -23,8 +23,8 @@ static void BM_Activation_Template_Sigmoid(benchmark::State& state) {
   Vector<double, 128> v = Vector<double, 128>::Random();
   Template::Sigmoid<128> s;
   for (auto _ : state) {
-    s.activation(v);
-    s.activation_der(v);
+    benchmark::DoNotOptimize(s.activation(v));
+    benchmark::DoNotOptimize(s.activation_der(v));
   }
 }
 
@@ -34,8 +34,8 @@ static void BM_Activation_Softmax(benchmark::State& state) {
   VectorXd v = VectorXd::Random(128);
   Softmax s;
   for (auto _ : state) {
-    s.function(v);
-    s.derivative(v);
+    benchmark::DoNotOptimize(s.function(v));
+    benchmark::DoNotOptimize(s.derivative(v));
   }
 }
 
@@ -45,8 +45,8 @@ static void BM_Activation_Template_Softmax(benchmark::State& state) {
   Vector<double, 128> v = Vector<double, 128>::Random();
   Template::Softmax<128> s;
   for (auto _ : state) {
-    s.activation(v);
-    s.activation_der(v);
+    benchmark::DoNotOptimize(s.activation(v));
+    benchmark::DoNotOptimize(s.activation_der(v));
   }
 }
 
@@ -56,8 +56,8 @@ static void BM_Activation_Relu(benchmark::State& state) {
   VectorXd v = VectorXd::Random(128);
   Relu r;
   for (auto _ : state) {
-    r.function(v);
-    r.derivative(v);
+    benchmark::DoNotOptimize(r.function(v));
+    benchmark::DoNotOptimize(r.derivative(v));
   }
 }
 
@@ -67,8 +67,8 @@ static void BM_Activation_Template_Relu(benchmark::State& state) {
   Vector<double, 128> v = Vector<double, 128>::Random();
   Template::Relu<128> r;
   for (auto _ : state) {
-    r.activation(v);
-    r.activation_der(v);
+    benchmark::DoNotOptimize(r.activation(v));
+    benchmark::DoNotOptimize(r.activation_der(v));
   }
 }
 
@@ -78,8 +78,8 @@ static void BM_Activation_Linear(benchmark::State& state) {
   VectorXd v = VectorXd::Random(128);
   Linear l;
   for (auto _ : state) {
-    l.function(v);
-    l.derivative(v);
+    benchmark::DoNotOptimize(l.function(v));
+    benchmark::DoNotOptimize(l.derivative(v));
   }
 }
 
@@ -89,8 +89,8 @@ static void BM_Activation_Template_Linear(benchmark::State& state) {
   Vector<double, 128> v = Vector<double, 128>::Random();
   Template::Linear<128> l;
   for (auto _ : state) {
-    l.activation(v);
-    l.activation_der(v);
+    benchmark::DoNotOptimize(l.activation(v));
+    benchmark::DoNotOptimize(l.activation_der(v));
   }
 }
 
